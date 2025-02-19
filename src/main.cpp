a2e100c7d4ce3233ed90bfe53782da1db0a7747d
2876225417 <2876225417@qq.com>
2025-02-19 19:58:55 +0800

    Add README[#00014].

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 
 #include <QApplication>
 #include <memory>
+#include <opencv2/imgcodecs.hpp>
 #include <qapplication.h>
 #include <windows/mainwindow.h>
 #include <utils/ort_inf.h>
@@ -11,6 +12,10 @@ int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
 
     ort_inferer* ort = new ort_inferer("model.onnx");
+
+    cv::Mat test = cv::imread("");
+
+    std::cout << "infer result: " << ort->infer(test);
 
     mainwindow main_mainwindow;
     main_mainwindow.show();
